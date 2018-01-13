bba4f5fdb9205e49532b1052575b330734db1d9f
MoreThanHidden <john816_@hotmail.com>
2018-01-13 18:00:41 +0800

    Version 0.6.3

--- src/main/java/morethanhidden/restrictedportals/RestrictedPortals.java
+++ src/main/java/morethanhidden/restrictedportals/RestrictedPortals.java
@@ -20,7 +20,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-@Mod(modid="restrictedportals", name="Restricted Portals", version="1.12-0.6.2")
+@Mod(modid="restrictedportals", name="Restricted Portals", version="1.12-0.6.3")
 public class RestrictedPortals {
 
 	@Mod.Instance(value = "restrictedportals")

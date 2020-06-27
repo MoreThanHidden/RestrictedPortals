ae0497cd98e7ab72ce7b775e5fded421b2d501a0
John Hannan <john816_@hotmail.com>
2020-06-28 02:30:07 +0800

    Show compatibility for ServerSide only use
    Fixes #22

--- src/main/java/morethanhidden/restrictedportals/RestrictedPortals.java
+++ src/main/java/morethanhidden/restrictedportals/RestrictedPortals.java
@@ -6,10 +6,13 @@ import net.minecraft.advancements.Advancement;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -27,7 +30,8 @@ public class RestrictedPortals {
 	public RestrictedPortals() {
 
 		ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, ConfigHandler.spec);
-
+              ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+		
 		//Register Event Handler
 		EventHandler eventHandler = new EventHandler();
 		MinecraftForge.EVENT_BUS.register(eventHandler);
